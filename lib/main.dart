a2a954ce25163cd9098e24586d0a48d2f4f675a9
zahidayturan <21060607@stu.omu.edu.tr>
2023-03-07 13:57:41 +0300

    fetch deneme

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Text(
-      "Bismillahirrahmanirrahhim sadagallahulazim",
+      "Bismillahirrahmanirrahhim ::",
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
