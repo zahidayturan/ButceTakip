8be93ef145955271a6c6df779e23cf512e4cba7f
ibrahim ethem <21060671@stu.omu.edu.tr>
2023-03-10 21:44:31 +0300

    MyHomePage hataları düzeltildi.

--- lib/App/ButceKontrolApp.dart
+++ lib/App/ButceKontrolApp.dart
@@ -1,15 +1,29 @@
 import 'package:butcekontrol/UI/Generalinfo.dart';
 import 'package:butcekontrol/appbar.dart';
+import 'package:butcekontrol/constans/MaterialColor.dart';
 import 'package:flutter/material.dart';
 
-class ButceKontrolApp extends StatefulWidget {
+class ButceKontrolApp extends StatelessWidget {
   const ButceKontrolApp({Key? key}) : super(key: key);
 
   @override
-  State<ButceKontrolApp> createState() => _ButceKontrolAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Bütçe Kontrol Uygulaması",
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: const MyHomePage(),
+    );
+  }
 }
-
-class _ButceKontrolAppState extends State<ButceKontrolApp> {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State <MyHomePage> {
+  CustomColors renkler = CustomColors();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
