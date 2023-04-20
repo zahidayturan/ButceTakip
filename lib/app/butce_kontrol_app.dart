27e239e2a0932dfbe9169704aaec89b6a14e9a77
ibrahim ethem <21060671@stu.omu.edu.tr>
2023-04-20 17:33:10 +0300

    giris ekran ve app ismi duzenlenmesi

--- lib/app/butce_kontrol_app.dart
+++ lib/app/butce_kontrol_app.dart
@@ -18,7 +18,7 @@ class ButceKontrolApp extends StatelessWidget {
       ],
       locale: const Locale('tr'),
       debugShowCheckedModeBanner: false,
-      title: "Bütçe Kontrol Uygulaması",
+      title: "Bütçe Takip",
       theme: ThemeData(
         primaryColor: Colors.white,
         fontFamily: "Nexa",
